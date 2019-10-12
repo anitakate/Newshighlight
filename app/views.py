9b40c3a20b9ebfcb09854d5c049b61e9570357b9
HWaruguru <hannah.waruguru8@gmail.com>
2019-10-12 20:46:51 +0300

    Add article view

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 
 from app import app
-from .fetch_data import get_sources
+from .fetch_data import get_sources, get_articles
 
 
 @app.route('/')
@@ -10,6 +10,7 @@ def index():
     return render_template("index.html", sources=sources)
 
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+@app.route('/articles/<string:source_id>')
+def articles(source_id):
+    articles = get_articles(source_id)
+    return render_template('articles.html', articles=articles)
