83a742a884ba958a0da21c0179363b3d7fc896c4
HWaruguru <hannah.waruguru8@gmail.com>
2019-10-12 18:58:07 +0300

    Add source model and process sources_data

--- app/fetch_data.py
+++ app/fetch_data.py
@@ -1,5 +1,8 @@
 from app import app
-import requests, json
+import requests
+import json
+from .models import Source
+
 
 def get_sources():
     '''
@@ -9,4 +12,16 @@ def get_sources():
 
     res = requests.get(sources_url)
     sources_data = res.json().get('sources')
-    return sources_data
+    return process_sources(sources_data)
+
+
+def process_sources(sources_data):
+    '''
+    Function that converts source dict into source model
+    '''
+    sources = []
+    for source_data in sources_data:
+        source = Source(source_data['id'], source_data['name'], source_data['description'],
+                        source_data['url'], source_data['category'], source_data['language'], source_data['country'])
+        sources.append(source)
+    return sources
